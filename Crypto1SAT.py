4776effd44eb0cd9b2add470f0aeefc55d321664
Tiny Labs Inc <elliot@tinylabs.io>
2022-05-08 12:26:45 -0600

    Initial checkin

---
+++ Crypto1SAT.py
@@ -0,0 +1,222 @@
+#!/bin/env python3
+#
+# Simple utility that takes a CNF file
+# which represents the fixed relationship
+# between SR[0:47] <= Output bits
+# over a given fixed time (fixed for CNF).
+#
+# If a corresponding output stream is given
+# it will run cryptominisat to solve for
+# SR[0:47] state n cycles previous
+
+from pycryptosat import Solver
+import argparse
+import os
+import math
+import tempfile
+import shutil
+import subprocess
+import time
+
+# Convenience class to represent array slices and convert
+# between various encodings
+class CNFArray:
+    def __init__(self, val, length=0):
+        self._val = []
+
+        # Handle text seed known input
+        if isinstance(val, str):
+            val = int (val, 0)
+        if isinstance(val, int):
+            # Round to nearest nibble
+            cnt = length
+            ncnt = math.ceil (cnt / 4) * 4
+            pad = ncnt - cnt
+
+            # If bit it set then set corresponding variable
+            for n in range (ncnt-1, pad-1, -1):
+                if val & (1 << n):
+                    self._val.append(1)
+                else:
+                    self._val.append(-1)
+
+        # Handle output from solver
+        if isinstance(val, tuple):
+            if isinstance (val[0], bool):
+                self._val = [1 if x else -1 for x in val]
+
+
+    def asList(self, offset):
+        _ = []
+        for x in self._val:
+            _.append(offset * x)
+            offset += 1
+        return _
+    
+    def __iter__(self):
+        self.idx = 0
+        return self
+
+    def __next__(self):
+        if self.idx < len (self._val):
+            return self._val[self.idx]
+        raise StopIteration
+
+    def __str__(self):
+        _ = ''
+        for n in self._val:
+            _ += str(n) + ' '
+        return _
+
+    def asBool(self):
+        _ = ''
+        for n in self._val:
+            _ += '1' if n > 0 else '0'
+        return _
+
+    def asHex(self):
+        x = 0
+        for n in self._val:
+            x <<= 1
+            if n > 0:
+                x |= 1 
+        return hex(x)
+
+class Cipher1Solver:
+    def __init__(self):
+        # Set on init
+        self.known_offset = 0
+        
+        # Set local vars
+        self.vrs = 0
+        self.eqs = 0
+        self.solver = None
+
+    # Takes array of known bits and generates possible keys
+    def Solve (self, cnf_array):
+        
+        # Solve equation with new assumption
+        sat, solution = self.solver.solve (assumptions=cnf_array.asList(self.known_offset))
+        key = CNFArray (solution[1:49])
+        return key
+        
+    # Parse the CNF into internal partial CNF without input bits
+    def ParseCNF(self, cnf_filename):
+
+        # Open file and parse
+        with open (cnf_filename, 'r') as f:
+
+            # Get header
+            a, b, self.vrs, equs = f.readline ().split (' ')
+            if (a != 'p') or (b != 'cnf'):
+                raise ValueError ('Invalid CNF header')
+
+            # Generate a new sat solver
+            self.solver = Solver (threads=2)
+            known = []
+            
+            # Parse each line
+            for line in f.readlines():
+
+                # Skip comment lines
+                if line[0] == 'c':
+                    continue
+
+                # parse out xor clauses
+                if line[0] == 'x':
+                    eq = [int(x) for x in line[1:].split (' ')[:-1]]
+                    if eq[0] < 0:
+                        eq[0] *= -1
+                        rhs = False
+                    else:
+                        rhs = True
+                else:
+                    eq = [int(x) for x in line.split (' ')[:-1]]
+
+                # If it's a literal add to known bits
+                if len (eq) == 1:
+                    known.append (eq[0])
+                # Add equation
+                elif line[0] == 'x':
+                    self.solver.add_xor_clause (eq, rhs)
+                    self.eqs += 1
+                else:
+                    self.solver.add_clause (eq)
+                    self.eqs += 1
+
+            # Get known offset
+            self.known_offset = abs (known[0])
+            
+            # Generate partial solution
+            start = time.time()
+            sat, solution = self.solver.solve ()
+            end = time.time()
+            print ('Solvable={} {}'.format (sat, end-start))
+            
+# Gen CNF problem
+def GenCNF (bitlen, shift):
+
+    # Change working directory
+    cdir = os.getcwd()
+    os.chdir (cdir + '/grainofsalt/build')
+    
+    # Calculate filename and path
+    filename = 'crypto1-0-' + str(bitlen) + '-' + \
+               str(shift) + '-0xe9fc41c974630-1.cnf'
+    abspath = os.getcwd() + '/satfiles/' + filename
+
+    # Call grainofsalt
+    args = ['./grainofsalt',
+            '--crypto=crypto1',
+            '--karnaugh=8',
+            #'--xorclauses',
+            '--nopropagate',
+            '--outputs', str(bitlen),
+            '--base-shift', str(shift)]
+    #print (args)
+    proc = subprocess.Popen (args, stdout = subprocess.PIPE)
+    if proc.stderr:
+        print (proc.stderr.readlines())
+
+    # Parse cnf to create pickle
+    cs = Cipher1Solver ()
+    cs.ParseCNF (abspath)
+    
+    # Cleanup
+    #print ('Cleaning up: {}'.format (abspath))
+    #shutil.rmtree(abspath)
+    
+    # Restore working directory
+    os.chdir (cdir)
+
+    # Return solver object
+    return cs
+
+# Parse args
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser ()
+
+    # Specify arguments
+    parser.add_argument ('--gen-bits', type=int,
+                        help='generate CNF given N observed bits')
+    parser.add_argument ('--shift', type=int,
+                         help='shift base SR before generating [0:49]')
+    parser.add_argument ('--output', type=int,
+                         help='file to write pickled CNF')
+    # Parse args
+    args = parser.parse_args ()
+
+    # Set defaults if not availble
+    if not args.shift:
+        args.shift = 0
+
+    # Call generator
+    if args.gen_bits:
+        solver = GenCNF (args.gen_bits, args.shift)
+        known = CNFArray ('0xe9fc41c974630', length=50)
+        print (known)
+        key = solver.Solve (known)
+        print (key.asHex())
+        #solver.Solve ('0x887b631497d0c')
+        #solver.Solve ('0x5a7be10a7259c')
+        #solver.Solve ('0x350b8357adb24')
--- README.md
+++ README.md
@@ -3,4 +3,18 @@ Python tools for the following:
 - Load CNF
 - Pass output bits and solve for possible keys
 
+// THis seems to work well for generation
+// outputs is variable
+./grainofsalt --outputs 50 --crypto crypto1 --karnaugh 8 --xorclauses --nopropagate
+
+When loading
+- Extract variable IDs of outputs
+- Extract variable IDs of SR[0][0:47]
+- Clauses that define output literals are removed on loading
+
+Solving
+- Observed outputs are passed to partially loaded model as literal clauses
+- SAT solver then solves for possible SR[0][0]-SR[0][47] state
+- Array of possible keys is produced
+
 
